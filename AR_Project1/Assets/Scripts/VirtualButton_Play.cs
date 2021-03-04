38e8f094253ed1813be65acbdda16e6cfb03a1b7
Jason Forral <Jason.Forral@gmail.com>
2021-03-03 19:45:57 -0800

    added button with no op script

---
+++ AR_Project1/Assets/Scripts/VirtualButton_Play.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton_Play : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject buttonObject;
+
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

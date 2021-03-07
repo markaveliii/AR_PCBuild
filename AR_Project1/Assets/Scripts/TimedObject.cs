b0b14ab978edc398364c0cfb27c259094b3ae59b
Jason Forral <Jason.Forral@gmail.com>
2021-03-06 21:07:38 -0800

    split script so each component controls timing

---
+++ AR_Project1/Assets/Scripts/TimedObject.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TimedObject : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Endpoint {
+    public double start;
+    public double end;
+    }
+
+    public Endpoint[] endpoints;
+
+    private int index;
+    private Renderer rend;
+    private VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = endpoints.Length - 1;
+        rend = GetComponent<Renderer>();
+        videoPlayer = GetComponentInParent<VideoPlayer>();
+
+        Debug.Log("do inactive objects run script?");
+        rend.enabled = false;
+        gameObject.layer = 8;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (index >= 0)
+        {
+            var time = videoPlayer.time;
+            var endpoint = endpoints[index];
+            if (time >= endpoint.start)
+            {
+                if (time >= endpoint.end)
+                {
+                    rend.enabled = false;
+                    gameObject.layer = 8;
+                    --index;
+                }
+                else
+                {
+                    rend.enabled = true;
+                    gameObject.layer = 0;
+                }
+            }
+
+        }
+    }
+}
